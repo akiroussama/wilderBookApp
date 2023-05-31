f3a28a30d97f38997e571928fe9bdd74597e633f
Akir Oussama <akir.oussama@gmail.com>
2023-05-31 08:26:26 +0200

    add decoractors to entities

--- back/src/entity/skill.ts
+++ back/src/entity/skill.ts
@@ -1,14 +1,19 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { Grade } from "./grade";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Skill {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   name: string;
 
+  @Field()
   @OneToMany(() => Grade, (grade) => grade.skill)
   public grades: Grade[];
 }
